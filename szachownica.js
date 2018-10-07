c70039b2507e78e618ed15bde286d62aa0e03871
Pawel-Kutny <43939127+Pawel-Kutny@users.noreply.github.com>
2018-10-08 00:00:41 +0200

    Add files via upload

---
+++ szachownica.js
@@ -0,0 +1,68 @@
+size(200, 200); 
+background(255); 
+stroke(0);
+noFill();
+smooth();
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (1+10*i, 1, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (11+10*i, 1, 10, 10);
+}for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (1+10*i, 11, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (11+10*i, 11, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (1+10*i, 21, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (11+10*i, 21, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (1+10*i, 31, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (11+10*i, 31, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (1+10*i, 41, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (11+10*i, 41, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (1+10*i, 51, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (11+10*i, 51, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (1+10*i, 61, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (11+10*i, 61, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (0);
+  rect (1+10*i, 71, 10, 10);
+}
+for (int i = 0; i<8; i= i+2){
+  fill (255);
+  rect (11+10*i, 71, 10, 10);
+}
